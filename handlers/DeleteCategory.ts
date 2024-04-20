d488f63f1dc821edfafd1ac5316d05acdffe1248
John DeLeon <makingcodeio@gmail.com>
2024-04-19 19:21:35 -0500

    Added Delete Category Logic

--- handlers/DeleteCategory.ts
+++ handlers/DeleteCategory.ts
@@ -1,3 +1,24 @@
 import {APIGatewayProxyEventV2, APIGatewayProxyResultV2, APIGatewayProxyHandlerV2} from 'aws-lambda'
 import {deleteCategory} from '../data/Category'
 
+export const main: APIGatewayProxyHandlerV2 = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> =>{
+    if(!event.pathParameters || !event.pathParameters.id){
+        return {
+            statusCode: 400,
+            body: JSON.stringify({message: 'Missing id'})
+        }
+    }
+    try{
+        const response = await deleteCategory(event.pathParameters.id);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response)
+        }
+    }catch(err){
+        console.log(err)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({message: 'Internal server error'})
+        }
+    }
+}
