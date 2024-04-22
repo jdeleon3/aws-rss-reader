8f8428123e6312f77a0f6614aae2047a5084ce1e
John DeLeon <makingcodeio@gmail.com>
2024-04-21 19:28:31 -0500

    updated process feed handler

--- handlers/ProcessRssFeed.ts
+++ handlers/ProcessRssFeed.ts
@@ -1,12 +1,25 @@
-import { fileURLToPath } from "url";
-import { SqsManager } from "../data/SqsManager";
-const SQS_QUEUE_URL:string|undefined = process.env.FEED_PROCESSING_QUEUE
+import {SQSEvent,SQSBatchItemFailure,SQSHandler, Context, SQSBatchResponse} from 'aws-lambda';
+import { processFeedRequest } from "../data/FeedItem";
 
 
-export const main = async(queueUrl:string|undefined=SQS_QUEUE_URL) =>{
-    
-    SqsManager.processRequests(queueUrl);
+export const main:SQSHandler = async(event:SQSEvent, context:Context):Promise<SQSBatchResponse> =>{
+    console.log(event);
+    const failures:SQSBatchItemFailure[] = [];
+    for(let record of event.Records){
+        try{
+            let feed = JSON.parse(record.body);
+            const sqsResponse = await processFeedRequest(feed);
+            console.log(sqsResponse);
+        }catch(err){
+            failures.push({itemIdentifier:record.messageId});
+        }
+    }
+    return {
+        batchItemFailures:failures
+    };
 }
+    
+    
 
 // Invoke main function if this file was run directly.
 //@ts-ignore
